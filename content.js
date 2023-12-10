51aadd4449bbca8d81a6bc789cd4946d3ffd3f32
edgarC <128599934+Hyeonnnnnn@users.noreply.github.com>
2023-12-11 02:09:57 +0800

    Update content.js

--- content.js
+++ content.js
@@ -5,52 +5,63 @@ let previouslyDisabledOption1 = null;
 let previouslyDisabledOption2 = null;
 
 select1.addEventListener('change', (event) => {
-   const selectedOption = event.target.value;
+  const selectedOption = event.target.value;
 
-   const options = select2.options;
-   for (let i = 0; i < options.length; i++) {
-     if (options[i].value === selectedOption) {
-       options[i].disabled = true;
-       break;
-     }
-   }
+  const options = select2.options;
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].value === selectedOption) {
+      options[i].disabled = true;
+      break;
+    }
+  }
 
-   if (previouslyDisabledOption1 !== null) {
-     previouslyDisabledOption1.disabled = false;
-   }
+  if (previouslyDisabledOption1 !== null) {
+    previouslyDisabledOption1.disabled = false;
+  }
 
-   previouslyDisabledOption1 = select2.querySelector(`[value="${selectedOption}"]`);
+  previouslyDisabledOption1 = select2.querySelector(`[value="${selectedOption}"]`);
 });
 
 select2.addEventListener('change', (event) => {
-   const selectedOption = event.target.value;
+  const selectedOption = event.target.value;
 
-   const options = select1.options;
-   for (let i = 0; i < options.length; i++) {
-     if (options[i].value === selectedOption) {
-       options[i].disabled = true;
-       break;
-     }
-   }
+  const options = select1.options;
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].value === selectedOption) {
+      options[i].disabled = true;
+      break;
+    }
+  }
 
-   if (previouslyDisabledOption2 !== null) {
-     previouslyDisabledOption2.disabled = false;
-   }
+  if (previouslyDisabledOption2 !== null) {
+    previouslyDisabledOption2.disabled = false;
+  }
 
-   previouslyDisabledOption2 = select1.querySelector(`[value="${selectedOption}"]`);
+  previouslyDisabledOption2 = select1.querySelector(`[value="${selectedOption}"]`);
 });
 
 function convert() {
-  var select1 = document.getElementById('select1').value;
-  var select2 = document.getElementById('select2').value;
-  var inputNum = document.getElementById('inputNum').value;
+  const select1Value = select1.value;
+  const select2Value = select2.value;
+  const inputNum = document.getElementById('inputNum').value;
 
-  if (select1 === select2) {
+  const isValidInput =
+    (select1Value === '1' && /^[01]+$/.test(inputNum)) ||
+    (select1Value === '2' && /^\d+$/.test(inputNum)) ||
+    (select1Value === '3' && /^[0-7]+$/.test(inputNum)) ||
+    (select1Value === '4' && /^[0-9A-Fa-f]+$/.test(inputNum));
+
+  if (!isValidInput) {
+    document.getElementById('convertedResult').value = "NaN";
+    return;
+  }
+
+  if (select1Value === select2Value) {
     document.getElementById('convertedResult').value = inputNum;
   } else {
-    var result;
+    let result;
 
-    switch (select1) {
+    switch (select1Value) {
       case '1': // Binary
         result = parseInt(inputNum, 2);
         break;
@@ -65,7 +76,7 @@ function convert() {
         break;
     }
 
-    switch (select2) {
+    switch (select2Value) {
       case '1': // Binary
         result = result.toString(2);
         break;
@@ -82,26 +93,4 @@ function convert() {
 
     document.getElementById('convertedResult').value = result;
   }
-}
-
-function updatePattern() {
-  var select1 = document.getElementById('select1').value;
-  var pattern;
-
-  switch (select1) {
-    case '1': // Binary
-      pattern = /^[01]+$/;
-      break;
-    case '2': // Decimal
-      pattern = /^\d+$/;
-      break;
-    case '3': // Octal
-      pattern = /^[0-7]+$/;
-      break;
-    case '4': // Hexadecimal
-      pattern = /^[0-9A-Fa-f]+$/;
-      break;
-  }
-
-  document.getElementById('inputNum').pattern = pattern;
 }
